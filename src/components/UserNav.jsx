13608cfbfa8806201dd0a5832038a04b7bbca643
sebsto93 <s.stobiecki93@gmail.com>
2024-10-12 17:39:52 +0200

    username

--- src/components/UserNav.jsx
+++ src/components/UserNav.jsx
@@ -1,15 +1,20 @@
 import { useDispatch } from "react-redux";
 import { logOut } from "../redux/auth/operations";
+import { useSelector } from "react-redux";
+import { selectUser } from "../redux/auth/selectors";
 
 export default function UserNav() {
   const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const userName = user?.name || "";
+
   const handleLoggOut = () => {
     dispatch(logOut());
   };
+
   return (
     <div>
-      <p>Welcome, nazwaUsera</p>
-      <button onClick={handleLoggOut}>Logout</button>
+      <p>Welcome, {userName}</p> <button onClick={handleLoggOut}>Logout</button>
     </div>
   );
 }
